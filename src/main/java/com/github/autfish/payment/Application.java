4ce9a5d1e8451b0718e0e1653ca34212a24adcd6
landongming <landongming@JDHGZ-LandmL.tcljr.com>
2020-02-18 14:20:27 +0800

    配置修改

--- src/main/java/com/github/autfish/payment/Application.java
+++ src/main/java/com/github/autfish/payment/Application.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class PaymentServiceApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(PaymentServiceApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
